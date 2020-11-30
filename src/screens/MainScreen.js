65720b1dc3004ecefea75251586b192391f2ef57
prostoAE <trident998@gmail.com>
2020-11-30 13:02:35 +0200

    033 Работа с картинками

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,24 +1,52 @@
 import React from 'react';
-import {FlatList, StyleSheet, View} from 'react-native';
+import {FlatList, StyleSheet, View, Image} from 'react-native';
 import {AddTodo} from "../components/AddTodo";
 import {Todo} from "../components/Todo";
 
 export const MainScreen = ({addTodo, todos, removeTodo, openTodo}) => {
+    let content = (
+        <FlatList
+            keyExtractor={item => item.id.toString()}
+            data={todos}
+            renderItem={({item}) => (
+                <Todo todo={item} onRemove={removeTodo} onOpen={openTodo}/>
+            )}
+        />
+    );
+
+    if (todos.length === 0) {
+        content = (
+            <View style={styles.imgWrap}>
+                <Image style={styles.image}
+                       source={require('../../assets/no-items.png')}
+                />
+                {/*<Image style={styles.image}
+                       source={{
+                           uri: 'https://reactnative.dev/img/tiny_logo.png',
+                       }}
+                />*/}
+            </View>
+        )
+    }
+
     return (
         <View>
             <AddTodo onSubmit={addTodo}/>
-
-            <FlatList
-                keyExtractor={item => item.id.toString()}
-                data={todos}
-                renderItem={({item}) => (
-                    <Todo todo={item} onRemove={removeTodo} onOpen={openTodo}/>
-                )}
-            />
+            {content}
         </View>
     )
 };
 
 const styles = StyleSheet.create({
-
+    imgWrap: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+        height: 300
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain'
+    }
 });

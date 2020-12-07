fc5b0c9b4802f2489a39c784036bc7266bc7106d
prostoAE <trident998@gmail.com>
2020-12-07 10:27:09 +0200

    037 Добавление иконок

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,45 +1,50 @@
 import React, {useState} from 'react';
 import {View, StyleSheet, TextInput, Button, Alert} from 'react-native';
+import {AntDesign} from "@expo/vector-icons";
+
 import {THEME} from "../theme";
 
 export const AddTodo = ({onSubmit}) => {
-    const [value, setValue] = useState('');
+  const [value, setValue] = useState('');
 
-    const pressHandler = () => {
-        if (value.trim()) {
-            onSubmit(value);
-            setValue('');
-        } else {
-            Alert.alert('Название дела не может быть пустым');
-        }
-    };
+  const pressHandler = () => {
+    if (value.trim()) {
+      onSubmit(value);
+      setValue('');
+    } else {
+      Alert.alert('Название дела не может быть пустым');
+    }
+  };
 
-    return (
-        <View style={styles.block}>
-            <TextInput style={styles.input}
-                       onChangeText={setValue}
-                       value={value}
-                       placeholder="Введите название дела..."
-                       autoCorrect={false}
-                       autoCapitalize="none"
-            />
-            <Button title='Добавить' onPress={pressHandler}/>
-        </View>
-    )
+  return (
+    <View style={styles.block}>
+      <TextInput style={styles.input}
+                 onChangeText={setValue}
+                 value={value}
+                 placeholder="Введите название дела..."
+                 autoCorrect={false}
+                 autoCapitalize="none"
+      />
+      <AntDesign.Button name='pluscircleo' onPress={pressHandler}>
+        Добавить
+      </AntDesign.Button>
+      {/*<Button title='Добавить' onPress={pressHandler}/>*/}
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
-    block: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 15
-    },
-    input: {
-        width: '70%',
-        borderStyle: 'solid',
-        borderBottomWidth: 2,
-        borderBottomColor: THEME.MAIN_COLOR,
-        padding: 10
-    }
+  block: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 15
+  },
+  input: {
+    width: '60%',
+    borderStyle: 'solid',
+    borderBottomWidth: 2,
+    borderBottomColor: THEME.MAIN_COLOR,
+    padding: 10
+  }
 });

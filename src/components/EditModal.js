532fc0cd8cca4d59f44eee16499789770c26fd51
prostoAE <trident998@gmail.com>
2020-12-08 11:01:29 +0200

    039 Управление клавиатурой

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -4,54 +4,54 @@ import {THEME} from "../theme";
 import {AppButton} from "./ui/AppButton";
 
 export const EditModal = ({visible, onCansel, value, onSave}) => {
-    const [title, setTitle] = useState(value);
+  const [title, setTitle] = useState(value);
 
-    const saveHandler = () => {
-        if (title.trim().length < 3) {
-            Alert.alert('Ошибка', `Минимальная длина названия 3 символа. Сейчас ${title.trim().length} символов.`)
-        } else {
-            onSave(title);
-        }
-    };
+  const saveHandler = () => {
+    if (title.trim().length < 3) {
+      Alert.alert('Ошибка', `Минимальная длина названия 3 символа. Сейчас ${title.trim().length} символов.`)
+    } else {
+      onSave(title);
+    }
+  };
 
-    return (
-        <Modal visible={visible} animationType='slide' transparent={false}>
-            <View style={styles.wrap}>
-                <TextInput
-                    value={title}
-                    onChangeText={setTitle}
-                    style={styles.input}
-                    placeholder='Введите название'
-                    autoCapitalize='none'
-                    autoCorrect={false}
-                    maxLength={64}
-                />
-                <View style={styles.buttons}>
-                    <AppButton onPress={onCansel} color={THEME.DANGER_COLOR}>Отментиь</AppButton>
-                    <AppButton onPress={saveHandler}>Сохранить</AppButton>
-                </View>
-            </View>
-        </Modal>
-    )
+  return (
+    <Modal visible={visible} animationType='slide' transparent={false}>
+      <View style={styles.wrap}>
+        <TextInput
+          value={title}
+          onChangeText={setTitle}
+          style={styles.input}
+          placeholder='Введите название'
+          autoCapitalize='none'
+          autoCorrect={false}
+          maxLength={64}
+        />
+        <View style={styles.buttons}>
+          <AppButton onPress={onCansel} color={THEME.DANGER_COLOR}>Отментиь</AppButton>
+          <AppButton onPress={saveHandler}>Сохранить</AppButton>
+        </View>
+      </View>
+    </Modal>
+  )
 };
 
 const styles = StyleSheet.create({
-    wrap: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+  wrap: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
 
-    },
-    input: {
-        padding: 10,
-        borderBottomColor: THEME.MAIN_COLOR,
-        borderBottomWidth: 2,
-        width: '80%'
-    },
-    buttons: {
-        width: '100%',
-        marginTop: 10,
-        flexDirection: 'row',
-        justifyContent: 'space-around'
-    }
+  },
+  input: {
+    padding: 10,
+    borderBottomColor: THEME.MAIN_COLOR,
+    borderBottomWidth: 2,
+    width: '80%'
+  },
+  buttons: {
+    width: '100%',
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  }
 });

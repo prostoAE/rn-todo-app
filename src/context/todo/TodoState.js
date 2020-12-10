def53a53f54e6b584afdae6b488776ebec9bdb26
prostoAE <trident998@gmail.com>
2020-12-10 11:31:07 +0200

    046 Создание редюсера

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -1,6 +1,7 @@
 import React, {useReducer} from "react";
 import {TodoContext} from "./todoContext";
 import {todoReducer} from "./todoReducer";
+import {ADD_TODO, REMOVE_TODO, UPDATE_TODO} from "../types";
 
 export const TodoState = ({children}) => {
   const initialState = {
@@ -10,7 +11,22 @@ export const TodoState = ({children}) => {
   };
   const [state, dispatch] = useReducer(todoReducer, initialState);
 
-  return <TodoContext.Provider value={{todos: state.todos}}>
-    {children}
-  </TodoContext.Provider>
+  const addTodo = title => dispatch({type: ADD_TODO, title});
+
+  const removeTodo = id => dispatch({type: REMOVE_TODO, id});
+
+  const updateTodo = (id, title) => dispatch({type:UPDATE_TODO, id, title});
+
+  return (
+    <TodoContext.Provider
+      value={{
+        todos: state.todos,
+        addTodo,
+        removeTodo,
+        updateTodo
+      }}
+    >
+      {children}
+    </TodoContext.Provider>
+  )
 };

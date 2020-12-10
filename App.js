242adf892f76f0fa98763c0b75d46b09c007ab53
prostoAE <trident998@gmail.com>
2020-12-10 10:44:01 +0200

    045 Рефакторинг приложения

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Alert} from 'react-native';
 import * as Font from "expo-font";
 import {AppLoading} from "expo";
 
-import {Navbar} from "./src/components/Navbar";
-import {MainScreen} from "./src/screens/MainScreen";
-import {TodoScreen} from "./src/screens/TodoScreen";
-import {THEME} from "./src/theme";
+import {MainLayout} from "./src/MainLayout";
+import {TodoState} from "./src/context/todo/TodoState";
 
 async function loadApplication() {
   await Font.loadAsync({
@@ -17,8 +14,6 @@ async function loadApplication() {
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
-  const [todoId, setTodoId] = useState(null);
-  const [todos, setTodos] = useState([]);
 
   if (!isReady) {
     return <AppLoading
@@ -28,79 +23,11 @@ export default function App() {
     />
   }
 
-  const addTodo = (title) => {
-    setTodos(prev => [...prev, {
-      id: Date.now().toString(),
-      title
-    }])
-  };
-
-  const removeTodo = (id) => {
-    const todo = todos.find(t => t.id === id);
-    Alert.alert(
-      "Удаление элемента",
-      `Вы уверенны, что хотите удалить "${todo.title}"?`,
-      [
-        {
-          text: "Отмена",
-          style: "cancel"
-        },
-        {
-          text: "Удалить",
-          onPress: () => {
-            setTodoId(null);
-            setTodos(prev => prev.filter(todo => todo.id !== id))
-          }
-        }
-      ],
-      {cancelable: false}
-    );
-  };
-
-  const updateTodo = (id, title) => {
-    setTodos(old => old.map(todo => {
-      if (todo.id === id) {
-        todo.title = title
-      }
-      return todo;
-    }))
-  };
-
-  let content = (
-    <MainScreen
-      todos={todos}
-      addTodo={addTodo}
-      removeTodo={removeTodo}
-      openTodo={setTodoId}
-    />
-  );
-
-  if (todoId) {
-    const selectedTodo = todos.find(todo => todo.id === todoId);
-
-    content = (
-      <TodoScreen
-        onRemove={removeTodo}
-        goBack={() => setTodoId(null)}
-        todo={selectedTodo}
-        onSave={updateTodo}
-      />
-    )
-  }
-
   return (
-    <View>
-      <Navbar title={'Todo App'}/>
-      <View style={styles.container}>
-        {content}
-      </View>
-    </View>
-  );
+    <TodoState>
+      <MainLayout/>
+    </TodoState>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: THEME.PADDING_HORISONTAL,
-    paddingVertical: 20
-  },
-});
+

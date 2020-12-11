f3711031807856c76632dbae4768e851bfd24ca7
prostoAE <trident998@gmail.com>
2020-12-11 13:23:06 +0200

    050 Рефакторинг приложения

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -1,15 +1,13 @@
-import React, {useState, useContext} from 'react';
+import React, {useContext} from 'react';
 import {View, StyleSheet, Alert} from "react-native";
 import {Navbar} from "./components/Navbar";
 import {THEME} from "./theme";
 import {MainScreen} from "./screens/MainScreen";
 import {TodoScreen} from "./screens/TodoScreen";
-import {TodoContext} from "./context/todo/todoContext";
 import {ScreenContext} from "./context/screen/screenContext";
 
 export const MainLayout = () => {
-  const {todos, addTodo, removeTodo, updateTodo} = useContext(TodoContext);
-  const {todoId, changeScreen} = useContext(ScreenContext);
+  const {todoId} = useContext(ScreenContext);
 
   // const removeTodo = (id) => {
   //   const todo = todos.find(t => t.id === id);
@@ -33,33 +31,11 @@ export const MainLayout = () => {
   //   );
   // };
 
-  let content = (
-    <MainScreen
-      todos={todos}
-      addTodo={addTodo}
-      removeTodo={removeTodo}
-      openTodo={changeScreen}
-    />
-  );
-
-  if (todoId) {
-    const selectedTodo = todos.find(todo => todo.id === todoId);
-
-    content = (
-      <TodoScreen
-        onRemove={removeTodo}
-        goBack={() => changeScreen(null)}
-        todo={selectedTodo}
-        onSave={updateTodo}
-      />
-    )
-  }
-
   return (
     <View>
       <Navbar title={'Todo App'}/>
       <View style={styles.container}>
-        {content}
+        {todoId ? <TodoScreen/> : <MainScreen/>}
       </View>
     </View>
   )
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,10 +1,14 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import {FlatList, StyleSheet, View, Image, Dimensions} from 'react-native';
 import {AddTodo} from "../components/AddTodo";
 import {Todo} from "../components/Todo";
 import {THEME} from "../theme";
+import {TodoContext} from "../context/todo/todoContext";
+import {ScreenContext} from "../context/screen/screenContext";
 
-export const MainScreen = ({addTodo, todos, removeTodo, openTodo}) => {
+export const MainScreen = () => {
+  const {addTodo, todos, removeTodo} = useContext(TodoContext);
+  const {changeScreen} = useContext(ScreenContext);
   const [deviceWidth, setDeviceWidth] = useState(
     Dimensions.get('window').width - THEME.PADDING_HORISONTAL * 2
   );
@@ -28,7 +32,7 @@ export const MainScreen = ({addTodo, todos, removeTodo, openTodo}) => {
         keyExtractor={item => item.id.toString()}
         data={todos}
         renderItem={({item}) => (
-          <Todo todo={item} onRemove={removeTodo} onOpen={openTodo}/>
+          <Todo todo={item} onRemove={removeTodo} onOpen={changeScreen}/>
         )}
       />
     </View>
